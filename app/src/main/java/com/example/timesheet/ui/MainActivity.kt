6a1ba50711e76bc469bf2734f4d4869861f3db40
Zhilin Yuri <yr.zhilin@gmail.com>
2023-03-12 21:29:30 +0500

    Added three fragments
    Добавил фрагменты (без реализации функционала и заполнения соответствующих файлов xml):
    -StartFragment
    -EmployeeListFragment
    -AddEmployeeFragment

--- app/src/main/java/com/example/timesheet/ui/MainActivity.kt
+++ app/src/main/java/com/example/timesheet/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.timesheet
+package com.example.timesheet.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.timesheet.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
