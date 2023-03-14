8fd65fc9632d158db65cb8f3a96dcb57c43c9d6d
Zhilin Yuri <yr.zhilin@gmail.com>
2023-03-14 20:47:34 +0500

    Added a DAO type object called "EmploeeDao"
    Создал объект типа DAO под названием "EmploeeDao" для взамодействием с сущностью "Emploee" при обращении к базе данных.

---
+++ app/src/main/java/com/example/timesheet/data/EmployeeDao.kt
@@ -0,0 +1,20 @@
+package com.example.timesheet.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EmployeeDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(employee: Employee)
+    @Update
+    suspend fun update(employee: Employee)
+    @Delete
+    suspend fun delete(employee: Employee)
+
+    @Query("SELECT * from employee WHERE id = :id")
+    fun getEmployee(id: Int): Flow<Employee>
+    @Query("SELECT * from employee ORDER BY lastName ASC")
+    fun getEmployees(): Flow<List<Employee>>
+}
