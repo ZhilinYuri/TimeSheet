8a29ce4d8f7ff83308707397e70fd3e2acf1dd9f
Zhilin Yuri <yr.zhilin@gmail.com>
2023-03-14 20:27:39 +0500

    Added entity called employee
    Создал пакет под названием "data" и добавил в него сущность "Employee".

---
+++ app/src/main/java/com/example/timesheet/data/Employee.kt
@@ -0,0 +1,33 @@
+package com.example.timesheet.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "employees"
+)
+data class Employee(
+    @PrimaryKey (autoGenerate = true)
+    val id:Long = 0,
+    //фамилия
+    @ColumnInfo(collate = ColumnInfo.NOCASE)
+    val lastName:String,
+    //имя
+    @ColumnInfo(collate = ColumnInfo.NOCASE)
+    val firstName:String,
+    //отчество
+    @ColumnInfo(collate = ColumnInfo.NOCASE)
+    val patronymic:String,
+    //должность
+    val position:String
+    /*
+    //дата рождения
+    @ColumnInfo(name = "age")
+    val dateOfBirth: Int,
+    //номер телефона
+    val phoneNumber:Int,
+    //адрес
+    val address:String
+    */
+)
