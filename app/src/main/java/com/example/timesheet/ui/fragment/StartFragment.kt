36921f29866c0c224f9b5d9a61b5cd92664bc815
Zhilin Yuri <yr.zhilin@gmail.com>
2023-03-17 21:55:11 +0500

    Implemented the StartFargment file and fragment_start.xml file
    Создал макет главного экран в файле fragment_start.xml и прописал логику обработчика нажатия на кнопку "buttonEmployees"

--- app/src/main/java/com/example/timesheet/ui/fragment/StartFragment.kt
+++ app/src/main/java/com/example/timesheet/ui/fragment/StartFragment.kt
@@ -5,21 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.timesheet.R
+import com.example.timesheet.databinding.FragmentStartBinding
 
 
 class StartFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var binding: FragmentStartBinding? = null
 
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+           buttonEmployees.setOnClickListener {
+               val action = StartFragmentDirections.actionStartFragmentToEmployeeListFragment()
+               findNavController().navigate(action)
+           }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
