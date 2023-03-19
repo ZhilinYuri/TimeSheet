c8bae99ab911cda492ab1079d580c3ad8e9b1633
Zhilin Yuri <yr.zhilin@gmail.com>
2023-03-19 21:59:36 +0500

    Implemented EmployeeListFragment.kt file
    Добавил всю необходимую бизнес-логику внутри класса EmployeeListFragment

--- app/src/main/java/com/example/timesheet/ui/fragment/EmployeeListFragment.kt
+++ app/src/main/java/com/example/timesheet/ui/fragment/EmployeeListFragment.kt
@@ -5,23 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.timesheet.R
-
+import com.example.timesheet.adapter.EmployeeListAdapter
+import com.example.timesheet.databinding.FragmentEmployeeListBinding
+import com.example.timesheet.viewModel.TimeSheetViewModel
 
 class EmployeeListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private val viewModel: TimeSheetViewModel by viewModels { TimeSheetViewModel.Factory }
+    private var _binding: FragmentEmployeeListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_employee_list, container, false)
+        _binding = FragmentEmployeeListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+        val adapter = EmployeeListAdapter {
+
+            val action = EmployeeListFragmentDirections.actionEmployeeListFragmentToEmployeeDetailFragment(it.id)
+            this.findNavController().navigate(action)
+
+        }
+        binding.recyclerView.adapter = adapter
+        viewModel.allEmployees.observe(this.viewLifecycleOwner) { books ->
+            books.let {
+                adapter.submitList(it)
+            }
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
+        binding.floatingActionButton.setOnClickListener {
+            val action = EmployeeListFragmentDirections.actionEmployeeListFragmentToAddEmployeeFragment(
+                getString(R.string.add_employee)
+            )
+            this.findNavController().navigate(action)
+        }
+    }
 }
